542509c86bea86edabde2e4280a5ef68476c7534
Laxman Veer <laxmanveer@quickenloans.com>
2022-08-05 22:33:56 -0400

    all dependency package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, //Retain last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true // Pauses recording actions and state changes when the extension window is not open
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
